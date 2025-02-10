577c8e0ab5382a6ffd3a3e14f4ba943ccde6fe9a
atkaridarshan04 <atkaridarshan04@gmail.com>
2025-02-10 12:05:45 +0530

    git add all

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from "next/link";
+
+const About = () => {
+    return (
+        <>
+            <div>this is about page</div>
+            <Link href={'/'}>Home</Link>
+        </>
+    )
+}
+export default About
